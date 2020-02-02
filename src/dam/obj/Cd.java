e7a8da1c1c3933645443a4e37315be633ad991c2
ManuC <56949815+IzantManuC@users.noreply.github.com>
2020-02-02 02:11:10 +0100

    Agrege nuevo paquete con el nombre correspondiente y actualizando datos

---
+++ src/dam/obj/Cd.java
@@ -0,0 +1,37 @@
+package dam.obj;
+
+import java.time.LocalDate;
+
+public class Cd {
+
+	private String nombre;
+	private LocalDate date;
+
+	public Cd(String nombre, int anio, int mes, int dia) {
+		this.date = LocalDate.of(anio, mes, dia);
+		this.nombre = nombre;
+
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+
+	@Override
+	public String toString() {
+		return "Cd [getNombre()=" + getNombre() + ", getDate()=" + getDate() + "]";
+	}
+
+}
---
+++ src/dam/obj/Representante.java
@@ -0,0 +1,77 @@
+package dam.obj;
+
+public class Representante {
+
+	private String nombre, apellido;
+	private int edad, id;
+	private static int id2 = 100;
+	private float sueldo;
+	private boolean representando;
+	private Grupo grupo;
+
+	public Representante(String nombre, String apellido, int edad, float sueldo, Grupo grupo) {
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.edad = edad;
+		this.sueldo = sueldo;
+		this.grupo = grupo;
+		id2++;
+		id = id2;
+		representando = true;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public float getSueldo() {
+		return sueldo;
+	}
+
+	public void setSueldo(float sueldo) {
+		this.sueldo = sueldo;
+	}
+
+	public Grupo getGrupo() {
+		return grupo;
+	}
+
+	public void setGrupo(Grupo grupo) {
+		this.grupo = grupo;
+	}
+
+	@Override
+	public String toString() {
+		return "Representante [nombre=" + nombre + ", apellido=" + apellido + ", edad=" + edad + ", sueldo=" + sueldo
+				+ ", representando=" + representando + ", grupo=" + grupo + "]";
+	}
+
+}
